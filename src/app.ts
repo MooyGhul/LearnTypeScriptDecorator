93782f5872795cfe6762056755f88f507cda65c1
panwangp <richard_pan@sfu.ca>
2022-07-17 20:40:20 -0700

    template+

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,35 @@ function Logger(logMsg: string) {
 
 }
 
+// render some template which should be some HTML code 
+//  into some place in the DOM, where I find this hookID
+function WithTemplate(template: string, hookId: string) {
+  // _ tells I awares it but I dont use it
+  return function(_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  }
+}
+
+function WithTemplate2(template: string, hookId: string) {
+  return function(constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent=p.name;
+    }
+  }
+}
+
 // @ is a special identifier TS recogonises
 //    it points to a function that is a decorator (Logger)
 // decorators execute before the class is defined, not when it is instantiated
 // you dont need to instantiate the class at all.
-@Logger('Logging PERSON...')
+// @Logger('Logging class PERSON...')
+@WithTemplate2('<h1>My Person Project</h1>','app')
 class Person {
   name = 'Max';
   
